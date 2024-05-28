2da00b739895c7be57b9d7bda0bac587e39dd87e
Test Kullanıcısı <Test@gmail.com>
2024-05-28 17:16:01 +0300

    Gerekli Herşey Yapıldı

--- script/script.js
+++ script/script.js
@@ -1,6 +1,14 @@
-let menuIcon=document.querySelector("#menu-icon");
-let navbar=document.querySelector(".navbar");
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle("bx-x")
+// Menü ikonunu seç ve 'menuIcon' değişkenine ata
+let menuIcon = document.querySelector("#menu-icon");
+
+// Navbar'ı seç ve 'navbar' değişkenine ata
+let navbar = document.querySelector(".navbar");
+
+// Menü ikonuna tıklama olayı ekle
+menuIcon.onclick = () => {
+    // Menü ikonuna 'bx-x' sınıfını ekle veya kaldır
+    menuIcon.classList.toggle("bx-x");
+    
+    // Navbar'a 'active' sınıfını ekle veya kaldır
     navbar.classList.toggle("active");
-}
+};
